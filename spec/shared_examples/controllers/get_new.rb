2a45aeac7e0736d2fccfad0c6a2aacd16516873c
Devon Winders <dwinders@canvas.is>
2016-11-03 09:57:27 -0600

    Add Shared Examples and Test Helpers

---
+++ spec/shared_examples/controllers/get_new.rb
@@ -0,0 +1,10 @@
+RSpec.shared_examples :get_new do
+  describe 'GET #new' do
+    it 'renders new view' do
+      process :new, method: :get
+
+      expect(response).to render_template(:new)
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
---
+++ spec/support/helpers/controller_helper.rb
@@ -0,0 +1,17 @@
+module ControllerHelper
+  def controller_model
+    described_class.controller_name.classify.constantize
+  end
+
+  def plural_controller_model_name
+    controller_model.model_name.plural
+  end
+
+  def controller_model_name
+    controller_model.model_name.singular
+  end
+end
+
+RSpec.configure do |config|
+  config.include ControllerHelper
+end
