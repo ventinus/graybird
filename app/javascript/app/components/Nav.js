951bbc5b00fd0e62418bba27f77bfeac583197a4
Jonathan Gray <jonathanceddy@gmail.com>
2018-03-16 15:43:54 -0700

    tightening nav styles

--- app/javascript/app/components/Nav.js
+++ app/javascript/app/components/Nav.js
@@ -19,23 +19,23 @@ class Nav extends PureComponent {
     return (
       <div className={`nav ${this.state.isOpen ? 'is-open' : ''}`}>
         <div className="nav__inner">
-          <div className="nav__logo">
-            <Link className='nav__logo__link' to={paths.HOME}>
+          <div className="nav__inner__logo">
+            <Link className='nav__inner__logo__link' to={paths.HOME}>
               <img
                 src={logo}
                 alt="Living Room Realty Logo"
               />
             </Link>
             {address.length > 0 &&
-              <div className="nav__logo__info">
+              <div className="nav__inner__logo__info">
                 <p className="type--c1 type--uppercase">{address}</p>
                 <p className="type--a2 type--uppercase">MLS# {rmls_number} - {status}</p>
               </div>
             }
           </div>
-          <button className="nav__burger" onClick={this._toggleMenu}>
+          <button className="nav__inner__burger" onClick={this._toggleMenu}>
             <span className="is-visually-hidden">Toggle menu visibility</span>
-            <span className="nav__burger__lines"></span>
+            <span className="nav__inner__burger__lines"></span>
           </button>
         </div>
         <nav className='nav__links'>
