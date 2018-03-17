1299a38c656e9d97e814a1e85974cc3af60c5bea
Jonathan Gray <jonathanceddy@gmail.com>
2018-03-16 21:02:09 -0700

    fix nav maxwidth

--- app/javascript/app/components/Nav.js
+++ app/javascript/app/components/Nav.js
@@ -18,35 +18,37 @@ class Nav extends PureComponent {
 
     return (
       <div className={`nav ${this.state.isOpen ? 'is-open' : ''}`}>
-        <div className="nav__inner">
-          <div className="nav__inner__logo">
-            <Link className='nav__inner__logo__link' to={paths.HOME}>
-              <img
-                src={logo}
-                alt="Living Room Realty Logo"
-              />
-            </Link>
-            {address.length > 0 &&
-              <div className="nav__inner__logo__info">
-                <p className="type--c1 type--uppercase">{address}</p>
-                <p className="type--a2 type--uppercase">MLS# {rmls_number} - {status}</p>
-              </div>
-            }
+        <div className="nav__body">
+          <div className="nav__inner">
+            <div className="nav__inner__logo">
+              <Link className='nav__inner__logo__link' to={paths.HOME}>
+                <img
+                  src={logo}
+                  alt="Living Room Realty Logo"
+                />
+              </Link>
+              {address.length > 0 &&
+                <div className="nav__inner__logo__info">
+                  <p className="type--c1 type--uppercase">{address}</p>
+                  <p className="type--a2 type--uppercase">MLS# {rmls_number} - {status}</p>
+                </div>
+              }
+            </div>
+            <button className="nav__inner__burger" onClick={this._toggleMenu}>
+              <span className="is-visually-hidden">Toggle menu visibility</span>
+              <span className="nav__inner__burger__lines"></span>
+            </button>
           </div>
-          <button className="nav__inner__burger" onClick={this._toggleMenu}>
-            <span className="is-visually-hidden">Toggle menu visibility</span>
-            <span className="nav__inner__burger__lines"></span>
-          </button>
+          <nav className='nav__links'>
+            <ul className='nav__links__list'>
+              <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#home'}}>Home / Video</Link></li>
+              <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#details'}}>Details</Link></li>
+              <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#gallery'}}>Gallery</Link></li>
+              <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#community'}}>Community</Link></li>
+              <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#contact'}}>Contact</Link></li>
+            </ul>
+          </nav>
         </div>
-        <nav className='nav__links'>
-          <ul className='nav__links__list'>
-            <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#home'}}>Home / Video</Link></li>
-            <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#details'}}>Details</Link></li>
-            <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#gallery'}}>Gallery</Link></li>
-            <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#community'}}>Community</Link></li>
-            <li className='nav__links__list__item'><Link className='type--b1 type--uppercase' to={{hash: '#contact'}}>Contact</Link></li>
-          </ul>
-        </nav>
       </div>
     )
   }
