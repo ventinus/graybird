caed084b38d022f0910e0802c7a348c33b29f695
Jonathan Gray <jonathanceddy@gmail.com>
2018-03-20 21:19:22 -0700

    starting graphql user mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,9 +1,9 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
+  field :createUser, Types::UserType do
     description "An example field added by the generator"
+    # argument
     resolve ->(obj, args, ctx) {
       "Hello World!"
     }
